def288310122fc0fbd20f4ed218791feba971c28
shitharu <106463068+shitharu@users.noreply.github.com>
2022-09-04 22:08:53 +0530

    GoCheeta Online Cab Service

---
+++ GoCheeta_Client/target/generated-sources/jaxws-wsimport/icbt/Driverr.java
@@ -0,0 +1,125 @@
+
+package icbt;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for driverr complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="driverr">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="branch" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="telno" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "driverr", propOrder = {
+    "branch",
+    "id",
+    "name",
+    "telno"
+})
+public class Driverr {
+
+    protected String branch;
+    protected int id;
+    protected String name;
+    protected int telno;
+
+    /**
+     * Gets the value of the branch property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getBranch() {
+        return branch;
+    }
+
+    /**
+     * Sets the value of the branch property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setBranch(String value) {
+        this.branch = value;
+    }
+
+    /**
+     * Gets the value of the id property.
+     * 
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets the value of the id property.
+     * 
+     */
+    public void setId(int value) {
+        this.id = value;
+    }
+
+    /**
+     * Gets the value of the name property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the value of the name property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Gets the value of the telno property.
+     * 
+     */
+    public int getTelno() {
+        return telno;
+    }
+
+    /**
+     * Sets the value of the telno property.
+     * 
+     */
+    public void setTelno(int value) {
+        this.telno = value;
+    }
+
+}
